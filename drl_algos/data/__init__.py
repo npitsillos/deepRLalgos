521c782da28df4bb851c4b698a9b6b5fcf5bdbe3
Lewis Boyd <lewis-boyd@live.co.uk>
2021-09-17 15:51:06 +0100

    Updated init.py to include new features

--- drl_algos/data/__init__.py
+++ drl_algos/data/__init__.py
@@ -1,3 +1,4 @@
-from drl_algos.data.replay_buffer import ReplayBuffer
+from drl_algos.data.replay_buffer import ReplayBuffer, EREReplayBuffer, EpisodicReplayBuffer
 from drl_algos.data.path_collector import MdpPathCollector
 from drl_algos.data.logging import Logger
+from drl_algos.data.my_logging import Logger as Logger2
