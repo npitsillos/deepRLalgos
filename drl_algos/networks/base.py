a6684e390658c15fe50a3f1189541a74932dad06
Lewis Boyd <lewis-boyd@live.co.uk>
2021-09-17 16:27:13 +0100

    Added improved layer initialisation, added option to not apply activation to final layer, renamed hidden_sizes and hidden_activation to layer_* due to changes made to other networks, added a GRU base

--- drl_algos/networks/base.py
+++ drl_algos/networks/base.py
@@ -48,28 +48,30 @@ class Mlp(Base):
 
     def __init__(
         self,
-        hidden_sizes,
+        layer_sizes,
         input_size,
-        hidden_activation=F.relu,
-        hidden_init=utils.fanin_init,
-        b_init_value=0.,
+        layer_activation=F.relu,
+        act_last_layer=True,
+        layer_init="fanin",
+        bias_init_value=0.,
         layer_norm=False,
         layer_norm_kwargs=None,
     ):
-        super().__init__(output_size=hidden_sizes[-1])
+        super().__init__(output_size=layer_sizes[-1])
 
         self.input_size = input_size
-        self.hidden_activation = hidden_activation
+        self.layer_activation = layer_activation
+        self.act_last_layer = act_last_layer # If false, no activation on last layer
         self.layer_norm = layer_norm
         self.fcs = []
         self.layer_norms = []
 
         in_size = input_size
-        for i, next_size in enumerate(hidden_sizes):
+        for i, next_size in enumerate(layer_sizes):
             fc = nn.Linear(in_size, next_size)
             in_size = next_size
-            hidden_init(fc.weight)
-            fc.bias.data.fill_(b_init_value)
+            utils.initialise(fc.weight, layer_init, layer_activation)
+            fc.bias.data.fill_(bias_init_value)
             self.fcs.append(fc)
             self.__setattr__("fc{}".format(i), fc)
 
@@ -80,9 +82,48 @@ class Mlp(Base):
 
     def forward(self, input):
         features = input
+        last_index = len(self.fcs)-1
         for i, fc in enumerate(self.fcs):
             features = fc(features)
-            if self.layer_norm and i < len(self.fcs) - 1:
+            if self.layer_norm and i < last_index:
                 features = self.layer_norms[i](features)
-            features = self.hidden_activation(features)
+            if i < last_index or self.act_last_layer:
+                features = self.layer_activation(features)
         return features
+
+
+class Gru(Base):
+    """Gru Network"""
+
+    def __init__(
+        self,
+        input_size,
+        layer_sizes,
+    ):
+        super().__init__(output_size=layer_sizes[-1])
+
+        self.input_size = input_size
+        self.grus = []
+
+        in_size = input_size
+        for i, next_size in enumerate(layer_sizes):
+            gru = nn.GRUCell(input_size=in_size, hidden_size=next_size)
+            self.grus.append(gru)
+            self.__setattr__("gru{}".format(i), gru)
+            in_size = next_size
+
+    def forward(self, input, hidden):
+        """ To extend to stacked GRU, need to take [batch_size, layers, hidden]
+        instead of [batch_size, hidden]. Note - would only use final hidden for
+        more processing but need rest of hiddens for subsequent calls.
+        """
+        if len(self.grus) > 1:
+            new_hiddens = []
+            for i, gru in enumerate(self.grus):
+                new_hidden = gru(input, hidden[:,i])
+                new_hiddens.append(new_hidden)
+                input = new_hidden
+            return torch.stack(new_hiddens, dim=1) # TODO - check this is correct
+        else:
+            return self.grus[0](input, hidden)
+        # return new_hiddens # shape should be [batch_size, layers, hidden]
