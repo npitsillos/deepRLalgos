1b230d2504c38209cc269b7e974a0a1b146d7839
Lewis Boyd <lewis-boyd@live.co.uk>
2021-09-17 15:48:14 +0100

    Added SumTree data structure which could be used to implement Priortised Experience Replay

--- drl_algos/data/replay_buffer.py
+++ drl_algos/data/replay_buffer.py
@@ -347,34 +347,96 @@ class EpisodicReplayBuffer(ReplayBuffer):
         )
         return batch
 
-    class EREReplayBuffer(ReplayBuffer):
+class EREReplayBuffer(ReplayBuffer):
 
-        def ere_batch(self, batch_size, n, num_minibatch, num_minibatches,
-                      min=5000):
-            # Order indices oldest to most recent
-            indices = np.arange(self._size)
-            ordered = np.concatenate((indices[self._top:], indices[:self._top]))
+    def ere_batch(self, batch_size, n, num_minibatch, num_minibatches,
+                  min=5000):
+        # Order indices oldest to most recent
+        indices = np.arange(self._size)
+        ordered = np.concatenate((indices[self._top:], indices[:self._top]))
 
-            # Calculate how many datapoints to sample from
-            emphasis = n**(num_minibatch*1000 / num_minibatches)
-            data_range = np.max((int(self._max_replay_buffer_size * emphasis), min))
+        # Calculate how many datapoints to sample from
+        emphasis = n**(num_minibatch*1000 / num_minibatches)
+        data_range = np.max((int(self._max_replay_buffer_size * emphasis), min))
 
-            # Sample from subset
-            indices = np.random.choice(
-                indices[-data_range:],
-                size=batch_size,
-                replace=self._replace or self._size < batch_size
-            )
-            if not self._replace and self._size < batch_size:
-                warnings.warn('Replace was set to false, but is temporarily set to true because batch size is larger than current size of replay.')
-            batch = dict(
-                observations=self._observations[indices],
-                actions=self._actions[indices],
-                rewards=self._rewards[indices],
-                terminals=self._terminals[indices],
-                next_observations=self._next_obs[indices],
-            )
-            for key in self._env_info_keys:
-                assert key not in batch.keys()
-                batch[key] = self._env_infos[key][indices]
-            return batch
+        # Sample from subset
+        indices = np.random.choice(
+            indices[-data_range:],
+            size=batch_size,
+            replace=self._replace or self._size < batch_size
+        )
+        if not self._replace and self._size < batch_size:
+            warnings.warn('Replace was set to false, but is temporarily set to true because batch size is larger than current size of replay.')
+        batch = dict(
+            observations=self._observations[indices],
+            actions=self._actions[indices],
+            rewards=self._rewards[indices],
+            terminals=self._terminals[indices],
+            next_observations=self._next_obs[indices],
+        )
+        for key in self._env_info_keys:
+            assert key not in batch.keys()
+            batch[key] = self._env_infos[key][indices]
+        return batch
+
+class SumTree:
+    write = 0
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tree = np.zeros(2 * capacity - 1)
+        self.data = np.zeros(capacity, dtype=object)
+        self.n_entries = 0
+
+    # update to the root node
+    def _propagate(self, idx, change):
+        parent = (idx - 1) // 2
+
+        self.tree[parent] += change
+
+        if parent != 0:
+            self._propagate(parent, change)
+
+    # find sample on leaf node
+    def _retrieve(self, idx, s):
+        left = 2 * idx + 1
+        right = left + 1
+
+        if left >= len(self.tree):
+            return idx
+
+        if s <= self.tree[left]:
+            return self._retrieve(left, s)
+        else:
+            return self._retrieve(right, s - self.tree[left])
+
+    def total(self):
+        return self.tree[0]
+
+    # store priority and sample
+    def add(self, p, data):
+        idx = self.write + self.capacity - 1
+
+        self.data[self.write] = data
+        self.update(idx, p)
+
+        self.write += 1
+        if self.write >= self.capacity:
+            self.write = 0
+
+        if self.n_entries < self.capacity:
+            self.n_entries += 1
+
+    # update priority
+    def update(self, idx, p):
+        change = p - self.tree[idx]
+
+        self.tree[idx] = p
+        self._propagate(idx, change)
+
+    # get priority and sample
+    def get(self, s):
+        idx = self._retrieve(0, s)
+        dataIdx = idx - self.capacity + 1
+
+        return (idx, self.tree[idx], self.data[dataIdx])
