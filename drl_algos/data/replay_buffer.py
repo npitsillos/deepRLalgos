f0fd9413cf657521f59be5ae8d208db5fc43fd7a
Lewis Boyd <lewis-boyd@live.co.uk>
2021-09-17 15:41:22 +0100

    Added EpisodicReplayBuffer to track entire episodes

--- drl_algos/data/replay_buffer.py
+++ drl_algos/data/replay_buffer.py
@@ -27,33 +27,36 @@ class ReplayBuffer(object):
         env_info_sizes = None,
         replace = True,
     ):
+        """Note - it is standard practice to sample with replace=True even
+        though it may sample a single experience multiple times within a single
+        batch. It is more computationally efficient and should have negligible
+        performance impact."""
         self.env = env
 
+        # Get info sizes if provided
         if env_info_sizes is None:
             if hasattr(env, 'info_sizes'):
                 env_info_sizes = env.info_sizes
             else:
                 env_info_sizes = dict()
 
+        # Get dimensions
         self._observation_dim = get_dim(env.observation_space)
         self._action_space = env.action_space
         self._action_dim = get_dim(env.action_space)
         self._max_replay_buffer_size = max_replay_buffer_size
+
+        # Create local 2D buffers
         self._observations = np.zeros((max_replay_buffer_size,
                                        self._observation_dim))
-        # It's a bit memory inefficient to save the observations twice,
-        # but it makes the code *much* easier since you no longer have to
-        # worry about termination conditions.
         self._next_obs = np.zeros((max_replay_buffer_size,
                                    self._observation_dim))
         self._actions = np.zeros((max_replay_buffer_size, self._action_dim))
-        # Make everything a 2D np array to make it easier for other code to
-        # reason about the shape of the data
+
         self._rewards = np.zeros((max_replay_buffer_size, 1))
-        # self._terminals[i] = a terminal was received at time i
         self._terminals = np.zeros((max_replay_buffer_size, 1), dtype='uint8')
-        # Define self._env_infos[key][i] to be the return value of env_info[key]
-        # at time i
+
+        # Set up env infos
         self._env_infos = {}
         for key, size in env_info_sizes.items():
             self._env_infos[key] = np.zeros((max_replay_buffer_size, size))
@@ -61,12 +64,13 @@ class ReplayBuffer(object):
 
         self._replace = replace
 
+        # Track for indexing
         self._top = 0
         self._size = 0
 
     def add_path(self, path):
-        """
-        Add a path to the replay buffer.
+        """Add a path to the replay buffer.
+
         This default implementation naively goes through every step, but you
         may want to optimize this.
         NOTE: You should NOT call "terminate_episode" after calling add_path.
@@ -107,6 +111,7 @@ class ReplayBuffer(object):
 
     def add_sample(self, observation, action, reward, next_observation,
                    terminal, env_info, **kwargs):
+        # Handle discrete/continuous
         if isinstance(self._action_space, Discrete):
            new_action = np.zeros(self._action_dim)
            new_action[action] = 1
@@ -172,3 +177,172 @@ class ReplayBuffer(object):
 
     def get_snapshot(self):
         return {}
+
+class EpisodicReplayBuffer(ReplayBuffer):
+    """This implementation is only valid for a SINGLE worker.
+
+    Episode terminals automatically detected so episode can be added as multiple
+    paths.
+
+    Multiple workers could easily be implemented by only adding completed
+    episodes although this may be impractical for long horizon environments.
+    Best way may be to have the PathCollector return [workers, paths] in a
+    consistent manner then have replay buffer track each worker's paths for
+    terminals.
+    """
+
+    def __init__(
+        self,
+        max_replay_buffer_size,
+        env,
+        max_path_len,
+        env_info_sizes=None,
+        replace=True,
+    ):
+        super().__init__(
+            max_replay_buffer_size=max_replay_buffer_size,
+            env=env,
+            env_info_sizes=env_info_sizes,
+            replace=replace
+        )
+        self.max_path_len = max_path_len
+        # Stores array of indices for each episode in the buffer
+        self.episodes = []
+
+    def add_path(self, path):
+        # Track index where each experience is stored
+        trajectory = []
+
+        # Continue last episode if not reached max_path_len or terminal
+        new_ep = True
+        if self._size > 0: # skip if first path ever added
+            if len(self.episodes[-1]) < self.max_path_len:
+                if not self._terminals[self._top - 1]:
+                    new_ep = False
+
+        # Update episodes indices to remove experiences about to be replaced
+        path_len = len(path["actions"])
+        clear_space = (self._size + path_len) - self._max_replay_buffer_size
+        while clear_space > 0:
+            ep = self.episodes[0]
+            if len(ep) <= clear_space:
+                # Free oldest episode
+                self.episodes.pop(0)
+                clear_space -= len(ep)
+            else:
+                # Free oldest experiences
+                self.episodes[0] = self.episodes[0][clear_space:]
+                break
+
+        # Add each experience to the buffer
+        for i, (
+                obs,
+                action,
+                reward,
+                next_obs,
+                terminal,
+                agent_info,
+                env_info
+        ) in enumerate(zip(
+            path["observations"],
+            path["actions"],
+            path["rewards"],
+            path["next_observations"],
+            path["terminals"],
+            path["agent_infos"],
+            path["env_infos"],
+        )):
+            trajectory.append(self._top)
+            self.add_sample(
+                observation=obs,
+                action=action,
+                reward=reward,
+                next_observation=next_obs,
+                terminal=terminal,
+                agent_info=agent_info,
+                env_info=env_info,
+            )
+
+        if new_ep:
+            # Add new trajectory to episodes array
+            self.episodes.append(trajectory)
+        else:
+            # Append trajectory onto end of most recent episode
+            self.episodes[-1] += trajectory
+        self.terminate_episode()
+
+    def random_batch(
+        self,
+        batch_size,
+        sequence_len=1,
+        strict_sequence_len=True
+    ):
+        # If sequence length is 1 then return default batch
+        if sequence_len == 1:
+            return super().random_batch(batch_size=batch_size)
+
+        if strict_sequence_len:
+            valid_eps = []
+            ep_lens = []
+            # Filter out episodes shorter than sequence_len
+            for index in range(len(self.episodes)):
+                ep_len = len(self.episodes[index])
+                if ep_len >= sequence_len:
+                    valid_eps.append(index)
+                    ep_lens.append(ep_len)
+        else:
+            # Sample from all episodes
+            valid_eps = [id for id in range(len(self.episodes))]
+            ep_lens = [len(ep) for ep in self.episodes]
+
+        # Probability of sampling an episode increases with episode length to
+        # preserve uniform sampling of experiences
+        # e.g., with uniform episode sampling experiences are more likely to be
+        # sampled if they are contained within a shorter episode
+        ep_sample_probs = np.array(ep_lens) / np.sum(ep_lens)
+        ep_indices = np.random.choice(
+            valid_eps,
+            size=batch_size,
+            replace=True,
+            p=ep_sample_probs,
+        )
+
+        # Sample trajectory randomly from each episode
+        episodes = [self.episodes[index] for index in ep_indices]
+        obs_sequences = []
+        action_sequences = []
+        reward_sequences = []
+        terminal_sequences = []
+        next_obs_sequences = []
+        for ep in episodes:
+            ep_len = len(ep)
+            index = np.random.choice(ep_len)
+            if strict_sequence_len:
+                # Clip index to ensure enough experiences sampled
+                start_index = np.clip(index, None, ep_len-sequence_len)
+                end_index = start_index + sequence_len
+            else:
+                start_index = np.clip(index-sequence_len, 0, None)
+                end_index = index+1
+            indices = np.array(ep[start_index: end_index])
+
+            # Track sequences
+            obs_sequences.append(self._observations[indices])
+            action_sequences.append(self._actions[indices])
+            reward_sequences.append(self._rewards[indices])
+            terminal_sequences.append(self._terminals[indices])
+            next_obs_sequences.append(self._next_obs[indices])
+
+            # TODO - Add support for env_info_keys
+            # for key in self._env_info_keys:
+            #     assert key not in sequence.keys()
+            #     sequence[key] = self._env_infos[key][indices]
+
+        batch = dict(
+            observations=np.stack(obs_sequences),
+            actions=np.stack(action_sequences),
+            rewards=np.stack(reward_sequences),
+            terminals=np.stack(terminal_sequences),
+            next_observations=np.stack(next_obs_sequences),
+        )
+        return batch
